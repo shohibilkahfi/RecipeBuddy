ec9e7d74d7d2bb076bcba2bfaf02e413d1356759
shohibilkahfi <shohibil@orenda.co.id>
2025-08-13 16:58:42 +0700

    Initial Commit

---
+++ RecipeBuddy/RecipeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBuddyApp.swift
+//  RecipeBuddy
+//
+//  Created by Orenda M1 on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeBuddyTests/RecipeBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBuddyTests.swift
+//  RecipeBuddyTests
+//
+//  Created by Orenda M1 on 13/08/25.
+//
+
+import Testing
+@testable import RecipeBuddy
+
+struct RecipeBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
